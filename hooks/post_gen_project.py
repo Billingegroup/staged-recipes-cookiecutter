e00c89dbe49e72769562d4a61dd88c9c1bb3e782
Sparky <59151395+Sparks29032@users.noreply.github.com>
2024-06-28 19:08:25 -0400

    Comparison

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -19,7 +19,7 @@ if "{{ cookiecutter.source }}" == "GitHub":
 tar_gz_dist = requests.get(source_url)
 sha256_hash = sha256(tar_gz_dist.content).hexdigest()
 
-if sha256_hash.strip() = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855":
+if sha256_hash.strip() == "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855":
     warnings.warn("SHA256 Hash Returned Emtpy File Hash, make sure your .tar.gz is uploaded to {{ cookiecutter.source }}!")
 
 # Read the main.yml file line by line
