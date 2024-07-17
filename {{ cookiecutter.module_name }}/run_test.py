0c049ef17092d3a113d85980ea377ea960786824
Sparks29032 <Sparks29032@gmail.com>
2024-07-17 06:47:35 -0400

    pytest run_test.py

--- {{ cookiecutter.module_name }}/run_test.py
+++ {{ cookiecutter.module_name }}/run_test.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
 
-import {{ cookiecutter.module_name }}.tests
+import pytest
+import sys
+import importlib
+from pathlib import Path
 
-assert {{ cookiecutter.module_name }}.tests.test().wasSuccessful()
+if __name__ == "__main__":
+    package_directory = '{{ cookiecutter.module_name }}'
+    module = importlib.import_module(package_directory)
+    module_path = Path(module.__file__).parent
+    test_location = module_path / 'tests'
+    exit_code = pytest.main([str(test_location), "-v"])
+    assert exit_code == 0
